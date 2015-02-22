f1499095b90e2724f09d71d0fd07bd84cbe42ebb
Anders Ditlevsen <AndersDit@gmail.com>
2015-02-22 14:06:34 +0100

    Routing config problems

--- app.module.js
+++ app.module.js
@@ -1,9 +1,27 @@
 (function(){
 
-	angular
-		.module("Main", ["Main.products", "Main.cart"])
-		.run(function($rootScope){
+var productCtrl = function($scope){
+	$scope.message = "This is my product page";
+}
+
+
+var app =	angular
+		.module("Main", ["Main.products", "Main.cart"]);
+
+/* Denne code block får resten af app'en til at ikke at virke
+app.config(function ($routeProvider) {
+	$routeProvider.when('/product',
+	{
+		templateUrl:"product.html",
+		controller:"productCtrl"
+	})
+})
+*/		
+
+app.controller("productCtrl", productCtrl)
+
+app.run(function($rootScope){
 			$rootScope.cartProducts = {};
-		});
+		})
 
 })();
